bc1c8d304998a261073606ea55b9dc4a65cd671d
ts3083 <ts04031@gmail.com>
2023-01-06 18:56:53 +0900

    [update] 직진, 우회전, 좌회전 신호 구현 (미완성)

--- Assets/script/Car.cs
+++ Assets/script/Car.cs
@@ -11,15 +11,16 @@ public class Car : MonoBehaviour
     // 차량 속도 선언 - 10, 15, 20
     private int[] speed = new int[] { 10, 15, 20 };
     // 현재 차량의 위치에 따른 속도가 다름 => current speed 변수 선언
-    private static int init_speed = 5;
-    public int current_speed = init_speed; // 초기 속도 5
+    private static int init_speed = 10;
+    public int current_speed = init_speed; // 초기 속도 10
 
     // 차량 정지 및 직진 신호
     bool forward_signal = false;
 
-    float timer;
+    public float timer = 0.0f;
     float Passtime;
     int waitingTime = 200;
+    private float beforeRotation; // 회전 이전의 rotation
     private Vector3 rotation;
     public int way = 4;
     public int turn = 3;
@@ -30,11 +31,14 @@ public class Car : MonoBehaviour
     public string[][] rsuSelect;
     public string[] a;
     private List<GameObject> carList; // scene에 존재하는 차량(SportsCar2) 저장, test
-    private float carDist; // 차량 사이의 거리
-    private float distLimit = 10.0f; // 차량 사이의 거리 제한
     private int speedLimit = 30; // 속도 제한
     private bool isRoad_30 = false; // 속도 제한 30km/h의 도로인 경우
     private bool isRoad_60 = false; // 속도 제한 60km/h의 도로인 경우
+    private bool isCarStopped = false;
+    private bool isCrossRoad = false;
+    private string direction;
+    private float lRotateFactor = 0.8f; // 사거리에서 좌회전시 회전량 결정 요소
+    private float rRotateFactor = 1.7f; // 사거리에서 우회전시 회전량 결정 요소
     private GameObject carBack;
 
     void Awake()
@@ -43,6 +47,7 @@ public class Car : MonoBehaviour
         startRotation = transform.eulerAngles;
         rigid = GetComponent<Rigidbody>();
         carBack = transform.GetChild(8).gameObject;
+        beforeRotation = transform.eulerAngles.y;
         //carBack.transform.localPosition = new Vector3(0, 0, -2);
         //carBack.transform.Translate(new Vector3(0, 0, -2), Space.Self);
     }
@@ -116,6 +121,8 @@ public class Car : MonoBehaviour
             Debug.Log("10 제한 도로 진입!");
             //isRoad_30 = true;
             BackTriggerSettingBySpeed(10);
+            direction = decideDirection(); // 진행방향 랜덤 결정
+            Debug.Log(direction);
         }
         // speed 15 제한 도로에 진입하는 경우
         else if (other.CompareTag("Limit15RoadEnter"))
@@ -123,6 +130,8 @@ public class Car : MonoBehaviour
             Debug.Log("15 제한 도로 진입!");
             //isRoad_30 = true;
             BackTriggerSettingBySpeed(15);
+            direction = decideDirection(); // 진행방향 랜덤 결정
+            Debug.Log(direction);
         }
         // 좁은 도로에서 탈출하는 경우 - 속도 0km/h
         else if(other.CompareTag("NarrowRoadExit"))
@@ -155,6 +164,14 @@ public class Car : MonoBehaviour
                 BackTriggerSettingBySpeed(init_speed);
             }
         }
+
+        if (other.CompareTag("CrossRoad"))
+        {
+            if (direction == "left" || direction == "right")
+            {
+                drive(direction);
+            }
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -166,8 +183,111 @@ public class Car : MonoBehaviour
         //}
         if (other.CompareTag("carBack"))
         {
-            BackTriggerSettingBySpeed(init_speed);
+            BackTriggerSettingBySpeed(speedLimit);
         }
+    }
+
+    private void drive(string direction)
+    {
+        if (beforeRotation == 0)
+        {
+            // 좌회전
+            if (direction == "left")
+            {
+                transform.eulerAngles -= Vector3.up * lRotateFactor;
+                if (rotation.y > 180 && rotation.y <= 270)
+                {
+                    transform.eulerAngles = new Vector3(0, 270, 0);
+                    beforeRotation = 270;
+                }
+            }
+            // 우회전
+            else if (direction == "right")
+            {
+                transform.eulerAngles += Vector3.up * rRotateFactor;
+                if (rotation.y > 90 && rotation.y <= 180)
+                {
+                    transform.eulerAngles = new Vector3(0, 90, 0);
+                    beforeRotation = 90;
+                }
+            }
+        }
+        else if (beforeRotation == 270)
+        {
+            // 좌회전
+            if (direction == "left")
+            {
+                transform.eulerAngles -= Vector3.up * lRotateFactor;
+                if (rotation.y > 90 && rotation.y <= 180)
+                {
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+                    beforeRotation = 180;
+                }
+            }
+            // 우회전
+            else if (direction == "right")
+            {
+                transform.eulerAngles += Vector3.up * rRotateFactor;
+                if (rotation.y > 0 && rotation.y <= 90)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    beforeRotation = 0;
+                }
+            }
+        }
+        else if (beforeRotation == 180)
+        {
+            // 좌회전
+            if (direction == "left")
+            {
+                transform.eulerAngles -= Vector3.up * lRotateFactor;
+                if (rotation.y > 0 && rotation.y <= 90)
+                {
+                    transform.eulerAngles = new Vector3(0, 90, 0);
+                    beforeRotation = 90;
+                }
+            }
+            // 우회전
+            else if (direction == "right")
+            {
+                transform.eulerAngles += Vector3.up * rRotateFactor;
+                if (rotation.y > 270 && rotation.y <= 360)
+                {
+                    transform.eulerAngles = new Vector3(0, 270, 0);
+                    beforeRotation = 270;
+                }
+            }
+        }
+        else if (beforeRotation == 90)
+        {
+            // 좌회전
+            if (direction == "left")
+            {
+                transform.eulerAngles -= Vector3.up * lRotateFactor;
+                if (rotation.y > 270 && rotation.y <= 360)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    beforeRotation = 0;
+                }
+            }
+            // 우회전
+            else if (direction == "right")
+            {
+                transform.eulerAngles += Vector3.up * rRotateFactor;
+                if (rotation.y > 180 && rotation.y <= 270)
+                {
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+                    beforeRotation = 180;
+                }
+            }
+        }
+    }
+
+    private string decideDirection()
+    {
+        string[] s = { "straight", "left", "right" };
+
+        return s[Random.Range(0, 3)];
     }
 
     private void BackTriggerSettingBySpeed(int speed_)

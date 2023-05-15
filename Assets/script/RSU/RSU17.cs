a988a1fc85bb6f2943d7afc9c8d96bc422be3b1a
HyungSuk Kim <kimhyungsuk982@gmail.com>
2023-05-15 16:03:15 +0900

    5월 15일
    1. RoadEnterTrigger, RoadExitTrigger, ObjectDetectTrigger 추가
    2. RSU fowarding 코드 추가

--- Assets/script/RSU/RSU17.cs
+++ Assets/script/RSU/RSU17.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RSU17 : MonoBehaviour
 {
     private int current_RSU = 17;
-    private float RSU_effectRange = 20f;        // RSU 영향 범위
+    //private float RSU_effectRange = 20f;        // RSU 영향 범위
 
     private Collider[] carList;     // RSU 영향 범위 내의 차량 리스트, 배열 내의 모든 오브젝트가 차량이 아님!
     private int carListNum;     // 차량 리스트 내의 차량 수, 배열 내의 모든 오브젝트가 차량이 아님!
@@ -21,7 +21,7 @@ public class RSU17 : MonoBehaviour
     private int next_RSU; // 다음 RSU
     private int line_num; // 차량 차선 번호
 
-    private float epsilon;
+    //private float epsilon;
 
     // [state(destination RSU) 수, action(neighbor RUS) 수], Demand Level [time, energy]
     public float[,,] Q_table = new float[5, stateNum, actionNum];       // Demand Level 1, [100, 0] / Demand Level 2, [75, 25] / Demand Level 3, [50, 50] / Demand Level 4, [25, 75] / Demand Level 5, [0, 100]
@@ -73,7 +73,7 @@ public class RSU17 : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        carList = Physics.OverlapSphere(transform.position, RSU_effectRange);       // RSU_effectRange 범위 내의 모든 오브젝트(Collider)를 가져옴
+        carList = Physics.OverlapSphere(transform.position, RSU_parameters.RSU_effectRange);       // RSU_effectRange 범위 내의 모든 오브젝트(Collider)를 가져옴
         carListNum = carList.Length;        // carList 배열의 크기
         ifDistInRange();
     }
@@ -145,10 +145,9 @@ public class RSU17 : MonoBehaviour
     {
         // 해당 action의 index 값 저장
         actionIndex = 0;
-        epsilon = GameObject.Find("RSU1").GetComponent<RSU1>().epsilon;
 
         // ϵ 확률로 무작위 action(negibor RSU)을 선택
-        if (Random.Range(0.00000f, 1.00000f) < epsilon)
+        if (Random.Range(0.00000f, 1.00000f) < RSU_parameters.epsilon)
         {
             // 무작위로 선택한 action(neighbor RSU)이 이전 RSU가 아닌 경우
             do
